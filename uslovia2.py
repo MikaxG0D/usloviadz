e6ca5274fec8a06cb6dcf8315b5720ee0f4ac59f
MikaxG0D <143862234+MikaxG0D@users.noreply.github.com>
2023-09-29 21:12:54 +0700

    Update uslovia2.py

--- uslovia2.py
+++ uslovia2.py
@@ -1,12 +1,13 @@
 a, b, c = int(input()), int(input()), int(input())
-if a > b:
-    if b > c:
-        print(b)
-elif b > a:
-    if a > c:
-        print(a)
-elif a > c:
-    if c > b:
-        print(c)
-elif a == b == c:
-    print("ERROR")
+mid = x1
+if a == b or b == c or a == c:
+    midd = 'Error'
+else:
+    if (b > a and b < c) or (b < a and b > c):
+        mid = b
+    elif (c > a and c < b) or (c < a and c > b):
+        mid = c
+    elif (a > b and a < c) or (a < b and a > c)
+        mid = a
+
+print(f'Среднее число - {mid}')
